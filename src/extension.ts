4ec026d52abf809eca8344e4c3f7aa36029826f1
Charles Jonas <charlie@callawaycloudconsulting.com>
2016-10-15 14:55:40 -0600

    Launch now allows selection of debug/logs/ files

--- src/extension.ts
+++ src/extension.ts
@@ -7,24 +7,38 @@ const initialConfigurations =
 		name: 'Apex-Debug',
 		type: 'apex',
 		request: 'launch',
-		logFile: '${workspaceRoot}/${command.AskForProgramName}',
+		logFile: '${command.AskForLogName}',
 		workspaceRoot: '${workspaceRoot}',
 		stopOnEntry: true
 	}];
 
 export function activate(context: vscode.ExtensionContext) {
 
-	let disposable = vscode.commands.registerCommand('extension.apex.getProgramName', () => {
-		return vscode.window.showInputBox({
-			placeHolder: "Please enter the name of a text file in the workspace folder",
-			value: "debug.log"
+	let disposable = vscode.commands.registerCommand('extension.apex.getLogName', () => {
+		return vscode.workspace.findFiles('**/debug/logs/*.log', '').then(
+		(result: vscode.Uri[]) => {
+			let items = new Array<string>()
+			for(let i = 0; i < result.length; i++){
+				let f = result[i];
+				items.push(f.path);
+			}
+
+			return vscode.window.showQuickPick(items, {
+						placeHolder: "Please select a log file"
+			});
+		},
+		(reason: any) => {
+			// output error
+			vscode.window.showErrorMessage(reason);
 		});
 	});
+
 	context.subscriptions.push(disposable);
 
 	context.subscriptions.push(vscode.commands.registerCommand('extension.apex.provideInitialConfigurations', () => {
 		return JSON.stringify(initialConfigurations);
 	}));
+
 }
 
 export function deactivate() {
