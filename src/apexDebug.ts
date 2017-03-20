7be3ec94635b7efe3e74c4ba92d56145533e4390
Charles Jonas <charlie@callawaycloudconsulting.com>
2017-03-20 16:00:30 -0600

    Added conditional breakpoints and reworked init to use async reads

--- src/apexDebug.ts
+++ src/apexDebug.ts
@@ -4,10 +4,10 @@
 import {
 	DebugSession, InitializedEvent,
 	TerminatedEvent, StoppedEvent, BreakpointEvent,
-	OutputEvent, Event, Thread, Scope, Handles, Breakpoint
+	OutputEvent, Event, Thread, Scope, Handles, Breakpoint, Source
 } from 'vscode-debugadapter';
 import {DebugProtocol} from 'vscode-debugprotocol';
-import {readFileSync} from 'fs';
+import {readFile} from 'fs';
 import {FrameProcessor} from './lib/frameProcessor';
 
 /**
@@ -24,6 +24,38 @@ export interface LaunchRequestArguments extends DebugProtocol.LaunchRequestArgum
 	traceLog?: boolean;
 }
 
+export class ApexBreakPoint extends Breakpoint{
+	id: number;
+	condition: String;
+	hitCount: number;
+	hitConditionModifer: String;
+	hitConditionValue: number;
+	line: number;
+
+
+	public meetsCondition():Boolean{
+		if(this.hitConditionValue){
+			switch(this.hitConditionModifer){
+				case '=':
+				case undefined:
+					return this.hitCount == this.hitConditionValue;
+				case '>':
+					return this.hitCount > this.hitConditionValue;
+				case '>=':
+					return this.hitCount >= this.hitConditionValue;
+				case '<':
+					return this.hitCount < this.hitConditionValue;
+				case '<=':
+					return this.hitCount <= this.hitConditionValue;
+				case '%':
+					return this.hitCount % this.hitConditionValue == 0;
+			}
+			return false;
+		}
+		return true;
+	}
+}
+
 export class ApexDebugSession extends DebugSession {
 
 	// we don't support multiple threads, so we can use a hardcoded ID for the default thread
@@ -38,7 +70,7 @@ export class ApexDebugSession extends DebugSession {
 	private _classPaths = new Map<string, string>();
 
 	// maps from sourceFile to array of Breakpoints
-	private _breakPoints = new Map<string, DebugProtocol.Breakpoint[]>();
+	private _breakPoints = new Map<string, ApexBreakPoint[]>();
 
 	private _variableHandles = new Handles<any>();
 
@@ -87,6 +119,8 @@ export class ApexDebugSession extends DebugSession {
 		// [TODO: Implement] make VS Code to use 'evaluate' when hovering over source
 		response.body.supportsEvaluateForHovers = false;
 
+		response.body.supportsHitConditionalBreakpoints = true;
+
 		this.sendResponse(response);
 	}
 
@@ -96,52 +130,72 @@ export class ApexDebugSession extends DebugSession {
 		this._projectRoot = args.workspaceRoot + '/'
 
 		//add file for apex
-		this._classPaths.set('_logFile', this._logFile);
+		new Promise(this.loadClassPaths())
+		.then(()=>{
+			this.loadLogLines()
+			.then((logLines)=>{
+				this._frameProcessor = new FrameProcessor(this, logLines, this._classPaths, this._variableHandles, args.traceLog);
+				if (args.stopOnEntry) {
+					this.sendResponse(response);
 
-		//create class paths map
-		let localStore =  JSON.parse(readFileSync(this._projectRoot + 'config/.local_store').toString());
-		for (var property in localStore) {
-			if (localStore.hasOwnProperty(property)) {
-				var metadata = localStore[property];
-				if(metadata.type == 'ApexClass' || metadata.type == 'ApexTrigger'){
-					let fileName = this._projectRoot + metadata.fileName.replace('unpackaged','src');
-					//add both id and classname for lookup
-					this._classPaths.set(metadata.id.substring(0, metadata.id.length - 3) , fileName);
-					this._classPaths.set(metadata.fullName , fileName);
+					// we stop on the first line
+					this.sendEvent(new StoppedEvent("entry", ApexDebugSession.THREAD_ID));
+				} else {
+					// we just start to run until we hit a breakpoint or an exception
+					this.continueRequest(<DebugProtocol.ContinueResponse> response, { threadId: ApexDebugSession.THREAD_ID });
 				}
-			}
-		}
+			})
+		})
 
-		let logLines = new Array<string>();
+	}
 
-		//check file
-		logLines = readFileSync(this._logFile).toString().split('\n');
-		if(logLines[0].indexOf('APEX_CODE,FINEST')==-1 || logLines[0].indexOf('SYSTEM,FINE')==-1){
-			this.log('WARNING: Log does not have proper levels. Unexpected behavoir will likely occur. Set Debug levels to `APEX_CODE=FINEST` && `SYSTEM=FINE` \n');
-		}
-		for(let i = 0; i < logLines.length; i++){
-			let s = logLines[i];
-			if(s.indexOf('*** Skipped') == 0){
-				this.log('WARNING: Log Was truncated due to length... Try reducing log levels \n');
-				// throw new TypeError('Log Was truncated due to length... Try reducing log levels');
-			}else if(s.indexOf('* MAXIMUM DEBUG LOG SIZE REACHED *') >= 0){
-				this.log('WARNING: Log Was truncated due to length... Try reducing log levels \n');
-				// throw new TypeError('Log Was truncated due to length... Try reducing log levels');
-			}
-		}
+	private loadLogLines():Promise<Array<string>>{
+		return new Promise((resolve, reject) => {
+			let logLines = new Array<string>();
 
-		this._frameProcessor = new FrameProcessor(this, logLines, this._classPaths, this._variableHandles, args.traceLog);
+			//check file
+			readFile(this._logFile, (err, data)=>{
+				logLines = data.toString().split('\n');
+				if(logLines[0].indexOf('APEX_CODE,FINEST')==-1 || logLines[0].indexOf('SYSTEM,FINE')==-1){
+					this.log('WARNING: Log does not have proper levels. Unexpected behavoir will likely occur. Set Debug levels to `APEX_CODE=FINEST` && `SYSTEM=FINE` \n');
+				}
+				for(let i = 0; i < logLines.length; i++){
+					let s = logLines[i];
+					if(s.indexOf('*** Skipped') == 0){
+						this.log('WARNING: Log Was truncated due to length... Try reducing log levels \n');
+						// throw new TypeError('Log Was truncated due to length... Try reducing log levels');
+					}else if(s.indexOf('* MAXIMUM DEBUG LOG SIZE REACHED *') >= 0){
+						this.log('WARNING: Log Was truncated due to length... Try reducing log levels \n');
+						// throw new TypeError('Log Was truncated due to length... Try reducing log levels');
+					}
+				}
+				resolve(logLines);
+			});
+		});
+	}
 
-		if (args.stopOnEntry) {
-			this.sendResponse(response);
+	private loadClassPaths(){
+		return (resolve, reject)=>{
+			this._classPaths.set('_logFile', this._logFile);
 
-			// we stop on the first line
-			this.sendEvent(new StoppedEvent("entry", ApexDebugSession.THREAD_ID));
-		} else {
-			// we just start to run until we hit a breakpoint or an exception
-			this.continueRequest(<DebugProtocol.ContinueResponse>response, { threadId: ApexDebugSession.THREAD_ID });
+			readFile(this._projectRoot + 'config/.local_store', (err, data)=>{
+				let localStore = JSON.parse(data.toString());
+				for (var property in localStore) {
+					if (localStore.hasOwnProperty(property)) {
+						var metadata = localStore[property];
+						if(metadata.type == 'ApexClass' || metadata.type == 'ApexTrigger'){
+							let fileName = this._projectRoot + metadata.fileName.replace('unpackaged','src');
+							//add both id and classname for lookup
+							this._classPaths.set(metadata.id.substring(0, metadata.id.length - 3) , fileName);
+							this._classPaths.set(metadata.fullName , fileName);
+						}
+					}
+				}
+				resolve();
+			});
 		}
 	}
+
 
 	protected setBreakPointsRequest(response: DebugProtocol.SetBreakpointsResponse, args: DebugProtocol.SetBreakpointsArguments): void {
 
@@ -149,35 +203,55 @@ export class ApexDebugSession extends DebugSession {
 		var clientLines = args.lines;
 
 		// read file contents into array for direct access
-		var lines = readFileSync(path).toString().split('\n');
+		readFile(path, (err,data)=>{
+			var lines = data.toString().split('\n');
+			var breakpoints = new Array<ApexBreakPoint>();
 
-		var breakpoints = new Array<Breakpoint>();
+			// verify breakpoint locations
+			for (var i = 0; i < clientLines.length; i++) {
+				var l = this.convertClientLineToDebugger(clientLines[i]);
+				var verified = false;
+				if (l < lines.length) {
+					const line = lines[l].trim();
+					// if a line is empty or starts with '+' we don't allow to set a breakpoint but move the breakpoint down
+					if (line.length == 0 || line.indexOf("{") == 0)
+						l++;
+					// if a line starts with '-' we don't allow to set a breakpoint but move the breakpoint up
+					if (line.indexOf("}") == 0)
+						l--;
+					verified = true;
+				}
 
-		// verify breakpoint locations
-		for (var i = 0; i < clientLines.length; i++) {
-			var l = this.convertClientLineToDebugger(clientLines[i]);
-			var verified = false;
-			if (l < lines.length) {
-				const line = lines[l].trim();
-				// if a line is empty or starts with '+' we don't allow to set a breakpoint but move the breakpoint down
-				if (line.length == 0 || line.indexOf("{") == 0)
-					l++;
-				// if a line starts with '-' we don't allow to set a breakpoint but move the breakpoint up
-				if (line.indexOf("}") == 0)
-					l--;
-				verified = true;
+				const bp = new ApexBreakPoint(verified, this.convertDebuggerLineToClient(l));
+				bp.id = this._breakpointId++;
+				bp.hitCount = 0;
+				for(var k = 0; k < args.breakpoints.length; k++){
+					let bpa = args.breakpoints[k];
+					if(bpa.line == clientLines[k]){
+						bp.condition = bpa.condition;
+						if(bpa.hitCondition){
+							const regex = /(>|>=|=|<|<=|%)?\s*([0-9]+)/g;
+							var match = regex.exec(bpa.hitCondition);
+							if(match){
+								bp.hitConditionModifer = match[1];
+								bp.hitConditionValue =  Number(match[2]);
+							}else{
+								bp.verified = false;
+							}
+						}
+					}
+				}
+
+				breakpoints.push(bp);
 			}
-			const bp = <DebugProtocol.Breakpoint> new Breakpoint(verified, this.convertDebuggerLineToClient(l));
-			bp.id = this._breakpointId++;
-			breakpoints.push(bp);
-		}
-		this._breakPoints.set(path, breakpoints);
+			this._breakPoints.set(path, breakpoints);
 
-		// send back the actual breakpoint positions
-		response.body = {
-			breakpoints: breakpoints
-		};
-		this.sendResponse(response);
+			// send back the actual breakpoint positions
+			response.body = {
+				breakpoints: breakpoints
+			};
+			this.sendResponse(response);
+		});
 	}
 
 	/**
@@ -193,8 +267,6 @@ export class ApexDebugSession extends DebugSession {
 			stackFrames: this._frameProcessor.getFrames().slice(0).reverse(),
 			totalFrames: this._frameProcessor.getFrames().length
 		};
-		console.log('Frame Count: ' + response.body.totalFrames);
-		// console.log(response.body.stackFrames);
 		this.sendResponse(response);
 	}
 
@@ -266,8 +338,7 @@ export class ApexDebugSession extends DebugSession {
 	}
 
 	protected continueRequest(response: DebugProtocol.ContinueResponse, args: DebugProtocol.ContinueArguments): void {
-
-		//advance the log without sending, each time a frame is updated, check if it has a breakpoint
+		// advance the log without sending, each time a frame is updated, check if it has a breakpoint
 		while(this._frameProcessor.hasLines()){
 			this._frameProcessor.setNextFrame();
 			if(this.checkBreakpoints()){
@@ -323,7 +394,6 @@ export class ApexDebugSession extends DebugSession {
 
 	//step over: run until stack size is same as current
 	protected nextRequest(response: DebugProtocol.NextResponse, args: DebugProtocol.NextArguments): void {
-
 		let currentDepth = this._frameProcessor.getFrames().length;
 		while(this._frameProcessor.hasLines()){
 			this._frameProcessor.setNextFrame();
@@ -355,7 +425,6 @@ export class ApexDebugSession extends DebugSession {
 	}
 
 	protected disconnectRequest(response: DebugProtocol.DisconnectResponse, args: DebugProtocol.DisconnectArguments): void {
-		// this.sendResponse(response);
 		super.disconnectRequest(response, args);
 	}
 
@@ -373,6 +442,7 @@ export class ApexDebugSession extends DebugSession {
 	/* === Private Methods === */
 
 	private checkBreakpoints(): boolean{
+
 		let currentFrame = this._frameProcessor.getCurrentFrame();
 		if(!currentFrame) return false;
 
@@ -382,7 +452,21 @@ export class ApexDebugSession extends DebugSession {
 			for(let i = 0; i < breakpoints.length; i++){
 				let breakpoint = breakpoints[i];
 				if(currentFrame.line == breakpoint.line){
-					return true;
+					if(breakpoint.condition){
+						var frameVars = this._frameProcessor.getFrameVariables(currentFrame.id.toString());
+						for(var j = 0; j < frameVars.length; j ++){
+							var variable = frameVars[j];
+							var expression = `${variable.name}=${variable.value}`;
+							if(expression == breakpoint.condition.replace(' ', '')){
+								return true;
+							}
+						}
+					}
+
+					breakpoint.hitCount++;
+					if(breakpoint.meetsCondition()){
+						return true;
+					}
 				}
 			}
 		}
